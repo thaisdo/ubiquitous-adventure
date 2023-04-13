4ee0003656f7dcdd0556df1324f3b564d1f1eb03
thai oliveira <thainarasdo.23@gmail.com>
2023-04-13 19:52:04 -0300

    considerando a função props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import Title from "./components/Title"
 import Nav from "./components/Nav"
 
+function Screen(props) {
+  <main>
+    {props.children}
+  </main>
+}
+
 function App() {
   return (
     //<Title /> nao usando o props
-    <div>
-      <Title text="Olá, world pela segunda vez"/>
-      <Title text="Bem vindo, " />
-      <Title />
-      <Nav items={[1,2,3,4,5]} />
-    </div>
+    //<div>
+    //  <Title text="Olá, world pela segunda vez"/>
+    //  <Title text="Bem vindo, " />
+    //  <Title />
+    //  <Nav items={[1,2,3,4,5]} />
+    //</div>
+    <Tela>
+     <Title text="Olá, world pela segunda vez"/>
+     <Title text="Bem vindo, " />
+     <Title />
+     <Nav items={[1,2,3,4,5]} />
+    </Tela>
   )
 }
 
